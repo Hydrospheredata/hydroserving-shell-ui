52448db7e696726dbbf3be8cbe8b5ab974947a94
Paul Mck <vixtir90@gmail.com>
2021-11-02 16:24:24 +0300

    feat(report): simple interfaces

--- src/app/features/report/report/report.component.ts
+++ src/app/features/report/report/report.component.ts
@@ -1,6 +1,39 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
-interface ReportMeta {}
+type SummaryGrade = 'excellent' | 'good' | 'fair' | 'bad';
+type ReportDetailsType = 'checks' | 'suspicious' | 'drift' | 'overallDrift';
+
+interface ChecksSummary {
+  percentOfFailedRecords: number;
+  grade: SummaryGrade;
+}
+interface SuspiciousRecordsSummary {
+  percentOfSuspiciousChecks: number;
+  grade: SummaryGrade;
+}
+interface DriftSummary {
+  drifted: boolean;
+  grade: SummaryGrade;
+}
+interface OverallDriftSummary {
+  drifted: boolean;
+  grade: SummaryGrade;
+}
+
+interface ReportDetails {
+  summary: {
+    checks: ChecksSummary;
+    suspicious: SuspiciousRecordsSummary;
+    drift: DriftSummary;
+    overallDrift: OverallDriftSummary;
+  };
+  records: {
+    checks: [];
+    suspicious: [];
+    drift: [];
+    overallDrift: [];
+  };
+}
 
 @Component({
   selector: 'hs-report',
@@ -10,8 +43,28 @@ interface ReportMeta {}
 })
 export class ReportComponent implements OnInit {
   reportsMeta = [];
+  selectedDetails: ReportDetailsType | null = null;
+
+  report: ReportDetails = {
+    summary: {
+      checks: { percentOfFailedRecords: 10, grade: 'fair' },
+      suspicious: { percentOfSuspiciousChecks: 30, grade: 'fair' },
+      drift: { drifted: false, grade: 'excellent' },
+      overallDrift: { drifted: true, grade: 'bad' },
+    },
+    records: {
+      checks: [],
+      suspicious: [],
+      drift: [],
+      overallDrift: [],
+    },
+  };
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  revealDetails(detailsType: ReportDetailsType) {
+    this.selectedDetails = detailsType;
+  }
 }
